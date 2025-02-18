7051291f1e61f3453bcba9ff5cb0333e36106032
Codefarmer <abadasamuelosp@gmail.com>
2025-02-18 23:03:56 +0100

    chore: update state to hold envs

--- src/state/state.ts
+++ src/state/state.ts
@@ -5,6 +5,8 @@ class GlobalState {
     maestroTerminal: vscode.Terminal | undefined;
     treeDataProvider: MaestroWorkBenchTreeViewProvider | undefined;
     fileWatcher: vscode.FileSystemWatcher | undefined;
+
+    public envOverrides: Map<string, { [key: string]: string }> = new Map();
 }
 
 export const globalState = new GlobalState();
